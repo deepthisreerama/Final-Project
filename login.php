4ca235c843356c2be9745c259570b0956ed549de
Deepthi Sreerama <ds742@afsconnect1.njit.edu>
2017-05-02 14:10:23 -0400

    Added CSS for login page

--- login.php
+++ login.php
@@ -1,14 +1,29 @@
 <html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
 <body>
-<h1> Sign In</h1>
+  <div id="clouds" >
+	<div class="cloud x1"></div>
+	<!-- Time for multiple clouds to dance around -->
+	<div class="cloud x2"></div>
+	<div class="cloud x3"></div>
+	<div class="cloud x4"></div>
+	<div class="cloud x5"></div>
+</div>
+<div class="container">
+<div id="login">
+<h1 class="heading-style"> Sign In</h1>
 <form method ="post" action="index.php">
-<strong>Username:</strong><input type="text" name="email" value=""/><br><br>
-<strong>Password:</strong><input type="password" name="password" value=""/><br><br>
+<strong class="text-label">Username:</strong><input style="color:#ea4c88 !important;" type="text" name="email" value=""/><br>
+<strong class="text-label">Password:</strong><input style="color:#ea4c88 !important;" type="password" name="password" value=""/><br>
 <input type="hidden" name="action" value="check_login"/><br>
 <input type="submit" value="Login"/>
 </form>
 <form action= "register.php">
  <input type="submit" value="Register"/>
 </form>
+</div>
+</div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,21 +1,45 @@
 <html>
+<head>
+<style>
+input[type="radio"]{
+    -webkit-appearance: radio;
+}
+</style>
+  <link rel="stylesheet" href="style.css">
+</head>
 <body>
- <h1> Sign Up</h1>
+ <div id="clouds" >
+	<div class="cloud x1"></div>
+	<!-- Time for multiple clouds to dance around -->
+	<div class="cloud x2"></div>
+	<div class="cloud x3"></div>
+	<div class="cloud x4"></div>
+	<div class="cloud x5"></div>
+</div>
+<div class="container" style="height:610px;">
+<div id="register">
+ <h1 style="padding-bottom: 30px;color: #ea4c88;">Sign Up</h1>
  <form method = 'post' action = 'index.php'>
-  <strong> First Name: </strong><input type="text" name="firstname" value="" required/> </br> </br>
-  <strong> Last Name: </strong><input type="text" name="lastname" value="" required/> </br> </br>
-  <strong> Email: </strong><input type="text" name="email" value="" required/> </br> </br>
-  <strong> Password: </strong><input type="password" name="password" value="" required/> </br></br>
-  <strong> Phone: </strong><input type="text" name="phone_num" value="" required/> </br> </br>
-  <strong> Birthday: </strong><input type="date" name="birthday" value="" required/> </br> </br>
-  <strong> Gender: </strong><input type="radio" name="gender" value="female">Female
+  <label class="reg-label"> First Name: </label><input type="text" name="firstname" value="" required/> </br> <br>
+ <label class="reg-label"> Last Name: </label><input type="text" name="lastname" value="" required/> </br> <br>
+  <label class="reg-label">  Email: </label><input type="text" name="email" value="" required/> </br> <br>
+ <label class="reg-label"> Password: </label><input type="password" name="password" value="" required/> </br><br>
+ <label class="reg-label">  Phone: </label><input type="text" name="phone_num" value="" required/> </br> <br>
+  <label class="reg-label">  Birthday: </label><input type="date" name="birthday" value="" required/> </br> <br>
+  <label class="reg-label">  Gender: </label>
+                            <input type="radio" name="gender" value="female">Female
                             <input type="radio" name="gender" value="male">Male
                             <input type="radio" name="gender" value="other">Others
+                            
+
   <input type="hidden" name="action" value="registrar" /></br></br>
-  <input type="submit" value="Register" />
+  <input id = "submit_style" type="submit" value="Register" />
  </form>
  <form action="login.php" method = "post">
-  <input type='submit' value='Login'/>
+  <input id = "submit_style" type='submit' value='Login'/>
+  
  </form>
+ </div>
+ </div>
 </body>
 </html>
